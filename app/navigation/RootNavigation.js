618b6fee536debadd41afbaadb3eb11078f367a7
tony dugué <duguetony@gmail.com>
2022-03-01 02:06:26 +0100

    initialisation Redux

--- app/navigation/RootNavigation.js
+++ app/navigation/RootNavigation.js
@@ -6,18 +6,26 @@ import { NavigationContainer } from "@react-navigation/native";
 import Home from "../screens/Home";
 import RestaurantDetail from "../screens/RestaurantDetail";
 
+// Redux
+import { Provider as ReduxProvider } from "react-redux"
+import configureStore from "../store/store";
+
+const store = configureStore()
+
 export default function RootNavigation() {
 
   const Stack = createStackNavigator()
   const screenOptions = { headerShown: false }
 
   return (
-    <NavigationContainer>
-       <Stack.Navigator initialRouteName="Home" screenOptions={screenOptions}>
-         <Stack.Screen name="Accueil" component={Home} />
-         <Stack.Screen name="Restaurant" component={RestaurantDetail} />
-       </Stack.Navigator>
-    </NavigationContainer>
+    <ReduxProvider store={store}>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home" screenOptions={screenOptions}>
+          <Stack.Screen name="Accueil" component={Home} />
+          <Stack.Screen name="Restaurant" component={RestaurantDetail} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </ReduxProvider>
   )
 }
 
---
+++ app/store/store.js
@@ -0,0 +1,10 @@
+import { createStore } from 'redux'
+
+import reducer from "./reducers/index"
+
+export default function configureStore(initialState) {
+  const store = createStore(reducer, initialState)
+  return store
+}
+
+
